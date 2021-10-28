b8c500a21cc628f08b5b55f364346ab93ad26e8d
estarriol <b07902103@ntu.edu.tw>
2021-10-28 14:41:40 +0800

    Reorder command in zsh section

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 set +x
 
 # zsh
-ln -sfn "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ln -sfn "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
