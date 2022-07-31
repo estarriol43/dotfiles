2c51c73f6eaaf55cdb74ffa8c47fb771643cacf1
Jian-Lin, Li <b07902103@ntu.edu.tw>
2022-07-31 12:43:50 +0800

    Update lualine

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -8,5 +8,24 @@ lualine.setup {
   options = { 
     theme  = "wombat" 
   },
+  sections = {
+    lualine_b = {'branch', 'diff'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+        shorting_target = 40,
+      }
+    },
+  },
+  inactive_sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+        shorting_target = 40,
+      }
+    },
+  },
 }
 
