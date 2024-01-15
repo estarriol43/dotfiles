1138d3672bb4bd0ea65f3f958decc8f855173cb4
Jian-Lin, Li <b07902103@ntu.edu.tw>
2024-01-15 23:37:11 +0800

    Remap nvim windows resize shortcut

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -20,10 +20,10 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-w><Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-w><Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-w><Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-w><Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
