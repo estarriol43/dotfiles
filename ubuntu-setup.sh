37ffc822a85bc0b3bc632d49d39e571a9930bc3e
Jian-Lin Li <lijianlin1999@gmail.com>
2024-06-11 11:48:31 +0800

    Add ubuntu-setup.sh

---
+++ ubuntu-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+packages="
+    vim
+    git 
+    fd-find
+    ripgrep
+    cscope
+    htop
+    python3
+    python3-pip
+    zsh
+"
+
+sudo apt update
+sudo apt install -y $packages
+
+cd /tmp
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+tar xavf nvim-linux64.tar.gz
+cd ./nvim-linux64
+sudo cp  -r ./* /usr/local/
+
+# git
+git config --global user.name "Jian-Lin Li"
+git config --global user.email "b07902103@ntu.edu.tw"
