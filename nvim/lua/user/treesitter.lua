0ad3056dbd6df308a59f92f7ef6f8d5def4331d0
Jian-Lin, Li <b07902103@ntu.edu.tw>
2024-06-16 23:46:15 +0800

    Refactor treesitter.lua

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,12 +1,19 @@
-local configs = require("nvim-treesitter.configs")
-configs.setup {
-  ensure_installed =  {"c", "cpp", "lua", "vim", "vimdoc", "python", "bash", "java"},
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
 }
+
+function M.config()
+  require("nvim-treesitter.configs").setup ({
+    auto_install = true,
+    ignore_install = { "" }, -- List of parsers to ignore installing
+    highlight = {
+      enable = true, -- false will disable the whole extension
+      disable = { "" }, -- list of language that will be disabled
+      additional_vim_regex_highlighting = true,
+    },
+    indent = { enable = true }
+  })
+end
+
+return M
