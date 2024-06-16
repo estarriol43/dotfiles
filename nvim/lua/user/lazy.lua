c05477d7bedd6b464fe81dee2ba2ee8d90c53deb
Jian-Lin, Li <b07902103@ntu.edu.tw>
2024-06-16 22:59:29 +0800

    Refactor lualine.lua

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -1,17 +1,17 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system ({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
-  }
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup {
+require("lazy").setup({
   spec = LAZY_PLUGIN_SPEC,
   ui = {
     border = "rounded",
@@ -20,4 +20,4 @@ require("lazy").setup {
     enabled = true,
     notify = true,
   },
-}
+})
--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,31 +1,30 @@
-local status_ok, lualine  = pcall(require, "lualine")
-if not status_ok then
-  vim.notify("lualine not installed")
-  return
-end
-
-lualine.setup {
-  options = { 
-    theme  = "wombat" 
-  },
-  sections = {
-    lualine_b = {'branch', 'diff'},
-    lualine_c = {
-      {
-        'filename',
-        path = 1,
-        shorting_target = 40,
-      }
-    },
-  },
-  inactive_sections = {
-    lualine_c = {
-      {
-        'filename',
-        path = 1,
-        shorting_target = 40,
-      }
-    },
-  },
+local M = {
+  "nvim-lualine/lualine.nvim",
 }
 
+function M.config()
+  require("lualine").setup({
+    theme  = "wombat",
+    sections = {
+      lualine_b = {'branch', 'diff'},
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+          shorting_target = 40,
+        }
+      },
+    },
+    inactive_sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+          shorting_target = 40,
+        }
+      },
+    },
+})
+end
+
+return M
